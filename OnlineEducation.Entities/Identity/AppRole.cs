11e9058cd94209dcc1daf1f9c2624610cd54a6ed
Yavuz SAV <yavuzsav07@gmail.com>
2020-06-30 16:12:30 +0300

    Added identity user & role

---
+++ OnlineEducation.Entities/Identity/AppRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineEducation.Entities.Identity
+{
+    public class AppRole : IdentityRole
+    {
+
+    }
+}
---
+++ OnlineEducation.Entities/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineEducation.Entities.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
