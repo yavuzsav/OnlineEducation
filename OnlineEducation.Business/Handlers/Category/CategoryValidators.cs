278758aeaea172ed6566f7488094574601f3085b
Yavuz SAV <yavuzsav07@gmail.com>
2020-07-09 21:51:00 +0300

    Added category validators

---
+++ OnlineEducation.Business/Handlers/Category/CategoryValidators.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using OnlineEducation.Business.Handlers.Category.Commands;
+
+namespace OnlineEducation.Business.Handlers.Category
+{
+    public class CreateCategoryValidator : AbstractValidator<CreateCategory.Command>
+    {
+        public CreateCategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+
+    public class EditCategoryValidator : AbstractValidator<EditCategory.Command>
+    {
+        public EditCategoryValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
