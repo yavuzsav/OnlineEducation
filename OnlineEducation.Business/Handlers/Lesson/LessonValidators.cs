4b289f76e7674d1fb6b3bf903afbdb2df9215959
Yavuz SAV <yavuzsav07@gmail.com>
2020-07-09 21:55:21 +0300

    Added lesson validators

---
+++ OnlineEducation.Business/Handlers/Lesson/LessonValidators.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using OnlineEducation.Business.Handlers.Lesson.Commands;
+
+namespace OnlineEducation.Business.Handlers.Lesson
+{
+    public class CreateLessonValidator : AbstractValidator<CreateLesson.Command>
+    {
+        public CreateLessonValidator()
+        {
+            RuleFor(x => x.CategoryId).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+
+    public class EditLessonValidator : AbstractValidator<EditLesson.Command>
+    {
+        public EditLessonValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.CategoryId).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
