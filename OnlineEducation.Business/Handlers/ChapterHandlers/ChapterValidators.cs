d30a571c510e4cfbfdbba270f2b55da047a84c2a
Yavuz SAV <yavuzsav07@gmail.com>
2020-07-12 19:50:37 +0300

    Added chapter validators

---
+++ OnlineEducation.Business/Handlers/ChapterHandlers/ChapterValidators.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using OnlineEducation.Business.Handlers.ChapterHandlers.Commands;
+
+namespace OnlineEducation.Business.Handlers.ChapterHandlers
+{
+    public class CreateChapterValidator : AbstractValidator<CreateChapter.Command>
+    {
+        public CreateChapterValidator()
+        {
+            RuleFor(x => x.LessonId).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Content).NotEmpty();
+        }
+    }
+
+    public class EditChapterValidator : AbstractValidator<EditChapter.Command>
+    {
+        public EditChapterValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.LessonId).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Content).NotEmpty();
+        }
+    }
+}
