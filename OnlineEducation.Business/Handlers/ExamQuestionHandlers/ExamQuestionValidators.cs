8a9dab9c2c66de768015a03ab5544211ad1b2d40
Yavuz SAV <yavuzsav07@gmail.com>
2020-07-16 22:42:12 +0300

    Added exam question validator

---
+++ OnlineEducation.Business/Handlers/ExamQuestionHandlers/ExamQuestionValidators.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using OnlineEducation.Business.Handlers.ExamQuestionHandlers.Commands;
+
+namespace OnlineEducation.Business.Handlers.ExamQuestionHandlers
+{
+    public class CreateExamQuestionValidator : AbstractValidator<CreateExamQuestion.Command>
+    {
+        public CreateExamQuestionValidator()
+        {
+            RuleFor(x => x.Content).NotEmpty();
+            RuleFor(x => x.Option1).NotEmpty();
+            RuleFor(x => x.Option2).NotEmpty();
+            RuleFor(x => x.Option3).NotEmpty();
+            RuleFor(x => x.Option4).NotEmpty();
+            RuleFor(x => x.CorrectAnswer).NotEmpty();
+        }
+    }
+
+    public class EditExamQuestionValidator : AbstractValidator<EditExamQuestion.Command>
+    {
+        public EditExamQuestionValidator()
+        {
+            RuleFor(x => x.Content).NotEmpty();
+            RuleFor(x => x.Option1).NotEmpty();
+            RuleFor(x => x.Option2).NotEmpty();
+            RuleFor(x => x.Option3).NotEmpty();
+            RuleFor(x => x.Option4).NotEmpty();
+            RuleFor(x => x.CorrectAnswer).NotEmpty();
+        }
+    }
+}
