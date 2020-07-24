852f063cb5e38d29e34381bf7c494055537cb7c2
Yavuz SAV <yavuzsav07@gmail.com>
2020-07-24 22:20:52 +0300

    Added question validator

---
+++ OnlineEducation.Business/Handlers/QuestionHandlers/QuestionValidators.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using OnlineEducation.Business.Handlers.QuestionHandlers.Commands;
+
+namespace OnlineEducation.Business.Handlers.QuestionHandlers
+{
+    public class CreateQuestionValidator : AbstractValidator<CreateQuestion.Command>
+    {
+        public CreateQuestionValidator()
+        {
+            RuleFor(x => x.LessonId).NotEmpty();
+        }
+    }
+}
