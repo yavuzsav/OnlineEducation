00b2ec93845a796c1ea5365fd57304abddf5d894
Yavuz SAV <yavuzsav07@gmail.com>
2020-07-24 14:16:41 +0300

    Added question and question image entity

---
+++ OnlineEducation.Entities/Entities/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OnlineEducation.Entities.Identity;
+
+namespace OnlineEducation.Entities.Entities
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsAnswerVideo { get; set; }
+
+        public Guid OwnerId { get; set; }
+        public AppUser Owner { get; set; }
+
+        public Guid LessonId { get; set; }
+        public Lesson Lesson { get; set; }
+
+        public QuestionImage QuestionImage { get; set; }
+    }
+}
---
+++ OnlineEducation.Entities/Entities/QuestionImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OnlineEducation.Entities.Entities
+{
+    public class QuestionImage
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public string PublicId { get; set; }
+
+        public Guid QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
