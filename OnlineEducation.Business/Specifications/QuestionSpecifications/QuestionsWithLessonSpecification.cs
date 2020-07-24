3036d36bdfdc7af7d0b048544ceb1dad65f8112d
Yavuz SAV <yavuzsav07@gmail.com>
2020-07-24 22:09:17 +0300

    Added, get questions

---
+++ OnlineEducation.Business/Specifications/QuestionSpecifications/QuestionsWithLessonSpecification.cs
@@ -0,0 +1,36 @@
+﻿using OnlineEducation.Core.PaginationHelper;
+using OnlineEducation.DataAccess.Specifications;
+using OnlineEducation.Entities.Entities;
+
+namespace OnlineEducation.Business.Specifications.QuestionSpecifications
+{
+    public class QuestionsWithLessonSpecification : BaseSpecification<Question>
+    {
+        public QuestionsWithLessonSpecification(string userId) : base(x => x.OwnerId == userId)
+        {
+        }
+
+        public QuestionsWithLessonSpecification(string userId, PaginationParams paginationParams) : base(x =>
+            x.OwnerId == userId)
+        {
+            AddInclude(x => x.Lesson);
+            ApplyPaging(paginationParams.PageSize * (paginationParams.PageIndex - 1), paginationParams.PageSize);
+
+            if (!string.IsNullOrEmpty(paginationParams.Sort))
+            {
+                switch (paginationParams.Sort)
+                {
+                    case "Asc":
+                        AddOrderBy(c => c.CreatedAt);
+                        break;
+                    case "Desc":
+                        AddOrderByDescending(c => c.CreatedAt);
+                        break;
+                    default:
+                        AddOrderBy(c => c.CreatedAt);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ OnlineEducation.Entities/Dtos/QuestionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OnlineEducation.Entities.Dtos
+{
+    public class QuestionDto
+    {
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsAnswerVideo { get; set; }
+        public string LessonName { get; set; }
+        public QuestionImageDto QuestionImageDto { get; set; }
+    }
+}
